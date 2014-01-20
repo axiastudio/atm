0867cf9db7e961fa27e76c62f25f4691f0ec5d46
Mario Alexandro Santini <alexmario74@gmail.com>
2014-01-20 22:49:10 +0100

    Aggiunto test con file allegati. Corretto tipo atto in Determine.

--- test/com/axiastudio/suite/plugins/atm/helper/helperTest.java
+++ test/com/axiastudio/suite/plugins/atm/helper/helperTest.java
@@ -1,12 +1,21 @@
 package com.axiastudio.suite.plugins.atm.helper;
 
+import com.axiastudio.suite.plugins.atm.AllegatoATM;
 import com.axiastudio.suite.plugins.atm.PubblicazioneATM;
 import com.axiastudio.suite.plugins.atm.helper.PutAttoHelper;
+import com.axiastudio.suite.plugins.atm.ws.ATMClient;
+
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+import java.util.Properties;
 
 /**
  * User: tiziano
@@ -27,12 +36,73 @@ public class helperTest {
         p.setInizioconsultazione(oggi);
         p.setDurataconsultazione(10);
         p.setRichiedente("Comune di Riva del Garda");
-        p.setTipoatto("determina");
+        p.setTipoatto("Determine");
 
         PutAttoHelper helper = new PutAttoHelper();
+        
+        Properties ctx = loadConfig();
+        
+		helper.setup(ctx.getProperty(ATMClient.USER_ID),
+				ctx.getProperty(ATMClient.PASSWORD),
+				ctx.getProperty(ATMClient.MAC_NAME),
+				ctx.getProperty(ATMClient.WSAKEY),
+				ctx.getProperty(ATMClient.ENDPOINT));
+
         helper.putAtto(p);
 
 
     }
 
+    private Properties loadConfig() {
+    	Properties p = new Properties();
+
+		try {
+			p.load(getClass().getResourceAsStream("putatto.properties"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return p;
+		
+	}
+
+	@Test
+    public void testPutAttoAllegato() throws Exception {
+    
+    	Calendar calendar = Calendar.getInstance(Locale.ITALIAN);
+        Date oggi = calendar.getTime();
+
+        PubblicazioneATM p = new PubblicazioneATM();
+        p.setTitolo("Primo tentativo");
+        p.setDescrizione("Primo tentativo di inserimento su Albo");
+        p.setInizioconsultazione(oggi);
+        p.setDurataconsultazione(10);
+        p.setRichiedente("Comune di Riva del Garda");
+        p.setTipoatto("Determine");
+
+        PutAttoHelper helper = new PutAttoHelper();
+        
+        Properties ctx = loadConfig();
+        
+		helper.setup(ctx.getProperty(ATMClient.USER_ID),
+				ctx.getProperty(ATMClient.PASSWORD),
+				ctx.getProperty(ATMClient.MAC_NAME),
+				ctx.getProperty(ATMClient.WSAKEY),
+				ctx.getProperty(ATMClient.ENDPOINT));
+        
+        List<AllegatoATM> allegati = new ArrayList<AllegatoATM>();
+        
+        AllegatoATM allegato = new AllegatoATM();
+        allegato.setTitoloallegato("Allegato di prova");
+        
+        allegato.setFileallegato(new File("allegato.txt"));
+        
+        allegati.add(allegato);
+        
+        helper.putAtto(p, allegati);
+        
+        Assert.assertTrue(true);
+        
+    }
 }
